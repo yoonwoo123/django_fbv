55698ea995326f51b401379e4f96546297e1ace5
yoonwoo <lkkjasd@naver.com>
2019-03-19 05:42:37 +0000

    form 추가 활용법

--- boards/forms.py
+++ boards/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Board
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 # class BoardForm(forms.Form):
 #     title = forms.CharField(label='제목', max_length=10, error_messages={'required': '제목을 반드시 입력해주세요.'})
@@ -24,3 +26,11 @@ class BoardForm(forms.ModelForm):
             'required': '내용을 반드시 입력해주세요.'
         }
     }
+    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', 'Submit'))
+    
