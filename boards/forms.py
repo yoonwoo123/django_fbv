a43a6f272b17149f7c1c8c0581480a063b4f5ea5
yoonwoo <lkkjasd@naver.com>
2019-03-19 04:53:41 +0000

    10|ModelForm 적용|

--- boards/forms.py
+++ boards/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from .models import Board
 
-class BoardForm(forms.Form):
-    title = forms.CharField(label='제목', max_length=10, error_messages={'required': '제목을 반드시 입력해주세요.'})
-    content = forms.CharField(label='내용', error_messages={'required': '내용을 반드시 입력해주세요.'},
-                                            widget=forms.Textarea(attrs={'placeholder': '내용을 입력해줘!',
-                                                                         'class': 'input-box'
-                                            }))
+# class BoardForm(forms.Form):
+#     title = forms.CharField(label='제목', max_length=10, error_messages={'required': '제목을 반드시 입력해주세요.'})
+#     content = forms.CharField(label='내용', error_messages={'required': '내용을 반드시 입력해주세요.'},
+#                                             widget=forms.Textarea(attrs={'placeholder': '내용을 입력해줘!',
+#                                                                          'class': 'input-box'
+#                                             })
+
+# modelform
+class BoardForm(forms.ModelForm):                                      
+    class Meta:
+        model = Board
+        fields = ['title', 'content']
--- boards/views.py
+++ boards/views.py
@@ -12,12 +12,11 @@ def create(request):
     if request.method == "POST":
         board_form = BoardForm(request.POST)
         if board_form.is_valid():
-            board = Board()
-            board.title = board_form.cleaned_data.get('title')
-            board.content = board_form.cleaned_data.get('content')
-            board.save()
-            # board.title = request.POST.get('title')
-            # board.content = request.POST.get('content')
+            # board = Board()
+            # board.title = board_form.cleaned_data.get('title')
+            # board.content = board_form.cleaned_data.get('content')
+            # board.save()
+            board = board_form.save()
             return redirect(board)
     else:
         board_form = BoardForm()
@@ -48,18 +47,19 @@ def edit(request, board_pk):
     # 2-1. POST 요청이면 (사용자가 form을 통해 데이터를 보내준 것.)
     if request.method == "POST":
         # 사용자 입력값(request.POST)을 BoardForm에 전달해주고, 
-        board_form = BoardForm(request.POST)
+        board_form = BoardForm(request.POST, instance=board)
         # 검증 (유효성 체크)
         if board_form.is_valid():
-            board.title = board_form.cleaned_data.get('title')
-            board.content = board_form.cleaned_data.get('content')
-            board.save()
+            # board.title = board_form.cleaned_data.get('title')
+            # board.content = board_form.cleaned_data.get('content')
+            # board.save()
+            board = board_form.save()
             return redirect(board)
     # 2-2: GET 요청이면 (수정하기 버튼을 눌렀을 때)
     else:
         # BoardForm을 초기화(사용자 입력값을 넣어준 상태)
         # board_form = BoardForm(initial=board.__dict__) 이렇게 작성해도 가능
-        board_form = BoardForm(initial={'title': board.title, 'content': board.content})
+        board_form = BoardForm(instance=board)
     # context에 담겨 있는 board_form은 아래와 같이 가능함.
     # 1 - POST 요청에서 검증에 실패하였을 때, 오류 메시지가 포함된 상태
     # 2 - GET 요청에서 초기화된 상태
