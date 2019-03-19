f1c98779feb3c0a383a69b91623cfa9901081138
yoonwoo <lkkjasd@naver.com>
2019-03-19 00:41:38 +0000

    05|CRUD-index|

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
+def index(request):
+    boards = Board.objects.order_by('-pk')
+    context = {'boards': boards}
+    return render(request, 'boards/index.html', context)
