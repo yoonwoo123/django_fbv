4e2c593c92d916e7b018d4e1b9e68c31b540d616
yoonwoo <lkkjasd@naver.com>
2019-03-19 00:25:58 +0000

    03|Board model 생성|

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Board
 # Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'hit', 'created_at', 'updated_at']
+admin.site.register(Board)
--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    hit = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'<Board ({self.id})> : {self.title}' 
